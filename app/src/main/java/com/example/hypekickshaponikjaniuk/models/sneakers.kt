abe2da54c081d2fbb1dd82c25076c3b6fcec93cd
Kornel <korneljaniuk@gmail.com>
2026-04-24 21:30:34 +0200

    Dodanie sneakers.kt

---
+++ app/src/main/java/com/example/hypekickshaponikjaniuk/models/sneakers.kt
@@ -0,0 +1,9 @@
+package com.example.hypekickshaponikjaniuk.models
+
+data class Sneakers(
+    val brand: String = "",
+    val modelName: String = "",
+    val releaseYear: Number = 0,
+    val reselPrice: Number = 0,
+    val imageUrl: String = ""
+)
