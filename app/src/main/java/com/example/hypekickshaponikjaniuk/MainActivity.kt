f7e3e848f83e12409a4aa642823b179fe764f46f
Kornel <korneljaniuk@gmail.com>
2026-04-24 22:47:07 +0200

    Dodanie adaptera, pojedynczego itemu i zapisanie logiki w MainActivity.kt

--- app/src/main/java/com/example/hypekickshaponikjaniuk/MainActivity.kt
+++ app/src/main/java/com/example/hypekickshaponikjaniuk/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.example.hypekickshaponikjaniuk
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.hypekickshaponikjaniuk.databinding.ActivityMainBinding
+import com.example.hypekickshaponikjaniuk.models.Sneakers
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    lateinit var shoesList: MutableList<Sneakers>
+    lateinit var adapter: ShoesAdapter
+    val db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,5 +31,63 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        shoesList = mutableListOf()
+
+        adapter = ShoesAdapter(this, shoesList)
+        binding.shoesGridView.adapter = adapter
+
+        val db = FirebaseFirestore.getInstance()
+
+        fetchDataFromCloud()
+
+        binding.shoesGridView.setOnItemClickListener{_, _, position, _ ->
+            val clickedShoe = shoesList[position]
+            Toast.makeText(this, "Kliknięty but: ${clickedShoe.brand}.", Toast.LENGTH_SHORT).show()
+        }
+    }
+    private fun fetchDataFromCloud(){
+        db.collection("shoes")
+            .get()
+            .addOnSuccessListener { documents ->
+                shoesList.clear()
+
+                for(document in documents){
+                    val brand = document.getString("brand") ?: "Nieznany but"
+                    val modelName = document.getString("modelName") ?: "Nieznany model"
+                    val releaseYear = document.getLong("releaseYear")?.toInt() ?: 0
+                    val resellPrice = document.getLong("resellPrice")?.toInt() ?: 0
+                    val imageUrl = document.getString("imageUrl") ?: ""
+
+                    val shoe = Sneakers(brand, modelName, releaseYear, resellPrice)
+                    shoesList.add(shoe)
+                }
+
+                adapter.notifyDataSetChanged()
+            }
+            .addOnFailureListener { exception ->
+                Log.e("FIREBASE_ERROR", "Błąd pobierania danych: ", exception)
+                Toast.makeText(this, "Błąd pobierania danych z chmury!", Toast.LENGTH_LONG).show()
+            }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/example/hypekickshaponikjaniuk/models/sneakers.kt
+++ app/src/main/java/com/example/hypekickshaponikjaniuk/models/sneakers.kt
@@ -3,7 +3,7 @@ package com.example.hypekickshaponikjaniuk.models
 data class Sneakers(
     val brand: String = "",
     val modelName: String = "",
-    val releaseYear: Number = 0,
-    val reselPrice: Number = 0,
+    val releaseYear: Int = 0,
+    val reselPrice: Int = 0,
     val imageUrl: String = ""
 )
