6c3d414efd5b885c15eb3b10b7d0cc326f37aea4
Szymon <szymon.haponik@cyfroweszkoly.pl>
2026-04-25 20:42:22 +0200

    [ADD] deleting and creating items in database

--- app/src/main/java/com/example/hypekickshaponikjaniuk/AdminPanelActivity.kt
+++ app/src/main/java/com/example/hypekickshaponikjaniuk/AdminPanelActivity.kt
@@ -9,10 +9,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.hypekickshaponikjaniuk.databinding.ActivityAdminPanelBinding
+import com.example.hypekickshaponikjaniuk.models.Sneakers
 import com.google.firebase.firestore.FirebaseFirestore
 
 class AdminPanelActivity : AppCompatActivity() {
     lateinit var shoesNameList: MutableList<String>
+    lateinit var shoesIdList: MutableList<String>
     lateinit var adapter: ArrayAdapter<String>
     val db = FirebaseFirestore.getInstance() // łączenie się z bazą danych fireStore
     private lateinit var binding: ActivityAdminPanelBinding
@@ -32,7 +34,7 @@ class AdminPanelActivity : AppCompatActivity() {
 
 
         shoesNameList = mutableListOf()// lista z nazwami butów do ListView
-
+        shoesIdList = mutableListOf() //lista z id do usuwania i aktualizowania butów
         fetchDataFromCloud()
 
         val shoeListView = binding.shoesListView
@@ -41,6 +43,38 @@ class AdminPanelActivity : AppCompatActivity() {
 
         binding.addToDatabaseButton.setOnClickListener { // dodawnie danych do bazy fireStore
 
+            val brand = binding.brandEditText.text.toString()
+            val model = binding.modelEditText.text.toString()
+            val releaseYear: Int = binding.releaseYearEditText.text.toString().toInt()
+            val resellPrice: Int = binding.resellPriceEditText.text.toString().toInt()
+            val imageUrl = binding.imageUrlEditText.text.toString()
+
+
+            db.collection("shoes").add(
+                Sneakers(
+                    brand, model, releaseYear, resellPrice, imageUrl
+                )
+            ).addOnSuccessListener {
+                Toast.makeText(this, "Successfully added item", Toast.LENGTH_SHORT).show()
+            }.addOnFailureListener { e ->
+                Toast.makeText(this, "Error while adding item", Toast.LENGTH_SHORT).show()
+            }
+
+            fetchDataFromCloud()
+        }
+
+
+
+        shoeListView.setOnItemLongClickListener{_, _, position, _ ->
+            db.collection("shoes").document(shoesIdList[position]).delete()
+                .addOnSuccessListener {
+                    Toast.makeText(this, "Succesfully deleted item", Toast.LENGTH_SHORT).show()
+                    fetchDataFromCloud()
+                }
+                .addOnFailureListener { e ->
+                    Toast.makeText(this, "Unexpected error occurd", Toast.LENGTH_SHORT).show()
+                }
+            true
         }
 
     }
@@ -50,11 +84,12 @@ class AdminPanelActivity : AppCompatActivity() {
             .get()
             .addOnSuccessListener { documents ->
                 shoesNameList.clear()
+                shoesIdList.clear()
 
                 for(document in documents){
                     val shoeName = document.getString("modelName")?: "Uknown shoe model name"
                     val shoePrice = document.getLong("resellPrice")?: -1
-
+                    shoesIdList.add(document.id)// pobranie id do usuwania przedmiotów
                     shoesNameList.add("$shoeName   $shoePrice $")
                 }
                 adapter.notifyDataSetChanged()
