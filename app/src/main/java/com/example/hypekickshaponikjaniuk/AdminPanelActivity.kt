83e2b4b3217b0132933faa39508576bf1fd4fed1
Szymon <szymon.haponik@cyfroweszkoly.pl>
2026-04-25 19:46:24 +0200

    [ADD] fetch data from fireStore database

--- app/src/main/java/com/example/hypekickshaponikjaniuk/AdminPanelActivity.kt
+++ app/src/main/java/com/example/hypekickshaponikjaniuk/AdminPanelActivity.kt
@@ -1,14 +1,20 @@
 package com.example.hypekickshaponikjaniuk
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.hypekickshaponikjaniuk.databinding.ActivityAdminPanelBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
 class AdminPanelActivity : AppCompatActivity() {
-
+    lateinit var shoesNameList: MutableList<String>
+    lateinit var adapter: ArrayAdapter<String>
+    val db = FirebaseFirestore.getInstance() // łączenie się z bazą danych fireStore
     private lateinit var binding: ActivityAdminPanelBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +30,40 @@ class AdminPanelActivity : AppCompatActivity() {
             insets
         }
 
+
+        shoesNameList = mutableListOf()// lista z nazwami butów do ListView
+
+        fetchDataFromCloud()
+
+        val shoeListView = binding.shoesListView
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, shoesNameList)
+        shoeListView.adapter = adapter
+
+        binding.addToDatabaseButton.setOnClickListener { // dodawnie danych do bazy fireStore
+
+        }
+
+    }
+
+        private fun fetchDataFromCloud() {
+        db.collection("shoes")
+            .get()
+            .addOnSuccessListener { documents ->
+                shoesNameList.clear()
+
+                for(document in documents){
+                    val shoeName = document.getString("modelName")?: "Uknown shoe model name"
+                    val shoePrice = document.getLong("resellPrice")?: -1
+
+                    shoesNameList.add("$shoeName   $shoePrice $")
+                }
+                adapter.notifyDataSetChanged()
+
+            }
+            .addOnFailureListener { exception ->
+                Log.e("FIREBASE_ERROR", "Błąd pobierania danych: ", exception)
+                Toast.makeText(this, "Error while loading data!", Toast.LENGTH_LONG).show()
+            }
 
     }
 }
